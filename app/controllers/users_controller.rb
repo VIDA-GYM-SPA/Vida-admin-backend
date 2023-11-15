cf35579601ec49360ec62b8f8daeb03b459b6408
Javier Diaz <javierdiazt406@gmail.com>
2023-11-15 14:06:16 -0400

    Fix ID of controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -84,7 +84,7 @@ class UsersController < ApplicationController
   private
 
   def find_user
-    @user = User.find_by_id!(params[:id])
+    @user = User.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       render json: { errors: 'User not found' }, status: :not_found
   end
